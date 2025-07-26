c5d904f749cd9b07d38088ef5c4bb75df784068a
Gui <augusto.g@outlook.com.br>
2025-07-26 17:10:16 -0300

    feat: criacao dos modelos de dados

---
+++ CadastraEmpresas/Models/Empresa.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CadastraEmpresas.Models
+{
+    public class Empresa
+    {
+        public int Id { get; set; }
+        public string NomeFantasia { get; set; }
+        [Required]
+        public string Cnpj { get; set; }
+        public string Situacao { get; set; }
+        public string Abertura { get; set; }
+        public string Tipo { get; set; }
+        public string NaturezaJuridica { get; set; }
+        public string AtividadePrincipal { get; set; }
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Municipio { get; set; }
+        public string Uf { get; set; }
+        public string Cep { get; set; }
+
+        public int UsuarioId { get; set; }
+        public Usuario Usuario { get; set; }
+    }    
+}
---
+++ CadastraEmpresas/Models/Usuario.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CadastraEmpresas.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O Nome e obrigatorio.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O e-mail e obrigatorio.")]
+        [EmailAddress(ErrorMessage = "Formato de e-mail invalido.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "A senha e obrigatoria")]
+        public string SenhaHash { get; set; }
+    }
+}
